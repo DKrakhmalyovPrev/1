36f9abdf226e767ebecd22445bb3991ff076f0ab
fredy4eg <fredy97@mail.ru>
2015-03-01 20:12:00 +0300

    Update book.h

--- book.h
+++ book.h
@@ -1,48 +1,36 @@
-#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+#include "book.h"
 
-class Book{
+class Sobranie{
 	public:
-		Book(){
-			printf("Введите название книги\n");
-			cin >> Name;
-			/*printf("Введите автора\n");
-			Avtor=(char*)malloc(30*sizeof(char));
-			while(c!='\n'){ 
-				scanf("%c", &c);
-				Avtor[i]=c;
-				i++;
-			};	
-			c='a';
-			i=0;
-			printf("Введите издательство\n");
-			Izdat=(char*)malloc(30*sizeof(char));
-			while(c!='\n'){ 
-				scanf("%c", &c);
-				Izdat[i]=c;
-				i++;
-			};	
-			c='a';
-			i=0;
-			printf("Введите год публикации\n");
-			scanf("%i", &year);
-			printf("Введите количество страниц\n");
-			scanf("%i", &pages);
-			printf("Введите содержание книги\n");
-			c='a';
-			i=0;
-			Soderj=(char*)malloc(300*sizeof(char));
-			if(Soderj==NULL){printf("NULL");};
-			while(c!='\n'){
-				scanf("%c", &c);
-				Soderj[i]=c;
-				i++;
-			};*/
-			printf("Данные получены\n");	
+		Sobranie(){
+			printf("Введите название собрания сочинений\n");
+			scanf("%s", Name);
+			printf("Введите автора\n");
+			scanf("%s", Avtor);
+			printf("Введите число томов\n");
+			scanf("%i", &toma);
+		};
+		void Addbook(){
+			Books=(Book*)malloc(toma*sizeof(Book));	
+			for(int i=0;i<toma;i++){
+				Book b;
+				Books[i] = b;
+			};
+		};
+		
 
+int year, toma, i,j,k,l; char c;
+char Avtor[30];
+char Name[30];
+Book *Books;
 };
-int year, pages, i,j,k,l; char c;
-string Avtor;
-string Name;
-string Izdat;
-string Soderj;
+
+int main(){
+	Sobranie odin;
+	odin.Addbook();
+	return(0);
 };
