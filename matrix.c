be892ab63012d1d5d2e2f27211b7d8cbc5721d2a
fredy4eg <fredy97@mail.ru>
2015-03-01 19:20:14 +0300

    Create matrix.c

---
+++ matrix.c
@@ -0,0 +1,36 @@
+#include"classm.h"
+
+
+/*void multiplicate(Matrix odin, Matrix dva)
+	{
+	int rasm, i,j;
+	int **tri;
+	rasm=odin.Rasmer();
+	tri=(int**)malloc(rasm*sizeof(int));
+	for(i=0;i<rasm;i++)
+		tri[i]=(int*)malloc(rasm*sizeof(int));
+	tri=odin*dva;
+	for (i=0;i<rasm;i++){
+		for(j=0;j<rasm;j++)
+			printf("%d ", tri[i][j]);
+		printf("\n");};
+	}
+void sum(Matrix odin, Matrix dva)
+	{
+	int rasm, i,j;
+	int **tri;
+	rasm=odin.Rasmer();
+	tri=(int**)malloc(rasm*sizeof(int));
+	for(i=0;i<rasm;i++)
+		tri[i]=(int*)malloc(rasm*sizeof(int));
+	tri=odin+dva;
+	for (i=0;i<rasm;i++){
+		for(j=0;j<rasm;j++)
+			printf("%d ", tri[i][j]);
+		printf("\n");};
+	}*/
+int main(){
+	int i,j;
+	Matrix odin;
+	return(0);
+};
