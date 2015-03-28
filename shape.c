7cccd554dfe0d830323e230f0f21e7b56cd38b0c
fredy4eg <fredy97@mail.ru>
2015-03-29 00:16:44 +0300

    Update shape.c

--- shape.c
+++ shape.c
@@ -4,33 +4,46 @@
 
 using namespace std;
 
-class Shape{
+class Shape
+{
 	int i;
 };
 
-class Point{
+class Point
+{
 	public:
-		Point(){
+		Point()
+		{
 			printf("Введите координаты точки\n");
 			scanf("%lf%lf", &x1,&y1);
 		}
+		
+		Point(double j,double k): x1(j), y1(k){};
+	
 	double x1,y1;
 };
 
-class Segment{
+class Segment
+{
 	public:
-		Segment(){
+		Segment()
+		{
 			printf("Введите точки отрезка\n");
 			scanf("%lf%lf%lf%lf", &x1,&y1,&x2,&y2);
 		}
-	void Lane(){
+		
+		Segment(double i,double j,double k,double l): x1(i), y1(j), x2(k), y2(l){};
+	
+	void Lane()
+	{
 		double k=(y2-y1)/(x2-x1);
 		double b=(x2*y1-x1*y2)/(x2-x1);
 		A=k;
 		B=-1;
 		C=-b;
 	}
-	int SegmentCut(Segment r,Segment s){
+	int SegmentCut(Segment r,Segment s)
+	{
 		double x,y;
 		r.Lane();
 		s.Lane();
@@ -42,38 +55,47 @@ class Segment{
 		else {return(1);};
 	}	
 	
-	void SegmentPoint(Segment seg, Point po){
+	void SegmentPoint(Segment seg, Point po)
+	{
 		seg.Lane();
 		if((seg.A*po.x1+seg.B*po.y1==seg.C)&&((po.x1-seg.x1)*(po.x1-seg.x2)<=0)){printf("Лежит");}else{printf("Не лежит");};
 	} 
 	
-	void LanePoint(Segment seg, Point po){
+	void LanePoint(Segment seg, Point po)
+	{
 		seg.Lane();
 		if(seg.A*po.x1+seg.B*po.y1>seg.C){printf("Ниже");};
 		if(seg.A*po.x1+seg.B*po.y1<seg.C){printf("Выше");}; 
 		if((seg.A*po.x1+seg.B*po.y1==seg.C)){printf("На прямой");};
-	};
+	}
 	double x1,x2,y1,y2, A, B, C;
 };
 
-class Vector{
+class Vector
+{
 	public:
-		Vector(){
+		Vector()
+		{
 			printf("Введите координаты начала и конца");
 			scanf("%lf%lf%lf%lf", &xst, &yst, &xend, &yend);
 		}
 
-		void Reverce(){
-			double i;
-			i=xst; xst=xend; xend=i;
-			i=yst; yst=yend; yend=i;
+		Vector(double i,double j,double k,double l): xst(i), yst(j), xend(k), yend(l){};
+	
+
+		Vector Reverce()
+		{
+			Vector s(xend,yend,xst,yst);
+			return(s);
 		}
 				
-		void VectorintoSegment(Vector a, Segment b){
+		void VectorintoSegment(Vector a, Segment b)
+		{
 			b.x1=a.xst;
 			b.x2=a.xend;
 			b.y1=a.yst;
 			b.y2=a.yend;
 		}
+
 double  xst, yst, xend, yend; 
 };
