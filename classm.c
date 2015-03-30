21afe4dde3c658df6cb544199c909737f7148ed7
fredy4eg <fredy97@mail.ru>
2015-03-30 20:53:43 +0300

    Update classm.c

--- classm.c
+++ classm.c
@@ -14,41 +14,38 @@ class Matrix
 {	
 	
 	private:
-		
-		int i,j;
-		element co,b,tr;
-	
-	public:
 		int wide;
 		element **table;
+			
+	public:
 		
 		Matrix(int s)
 		{
 			wide=s;
 			table=new element*[wide];
-			for(i=0;i<wide;i++)
+			for(int i=0;i<wide;i++)
 				table[i]=new element[wide];
-			for (i=0;i<wide;i++)
-				for(j=0;j<wide;j++)
-					table[i][j]=rand() % 300 - 150;
+			for (int i=0;i<wide;i++)
+				for(int j=0;j<wide;j++)
 					//cin>>table[i][j];
+					table[i][j]=rand() % 300 - 150;
 		};
 
 		Matrix(const Matrix<element>& a)
 		{
 			wide=a.wide;
 			table=new element*[wide];
-			for(i=0;i<wide;i++)
+			for(int i=0;i<wide;i++)
 				table[i]=new element[wide];
-			for(i=0;i<wide;i++)
-				for(j=0;j<wide;j++)
+			for(int i=0;i<wide;i++)
+				for(int j=0;j<wide;j++)
 					table[i][j]=a.table[i][j];
 		};
 
 		Matrix(int s, int i): wide(s)
 		{
 			table=new element*[wide];
-			for(i=0;i<wide;i++)
+			for(int i=0;i<wide;i++)
 				table[i]=new element[wide];
 		};
 
@@ -59,19 +56,19 @@ class Matrix
 		
 		void PrintTable()
 		{		
-			for(i=0;i<wide;i++)
+			for(int i=0;i<wide;i++)
 			{
-				for(j=0;j<wide;j++)
-					printf("%.2lf ", table[i][j]);
-					//cout<<table[i][j]<<" ";
-			cout<<"\n";
+				for(int j=0;j<wide;j++)
+					printf("%.2lf |", table[i][j]);
+					
+				cout<<"\n";			
 			}
 		};
 		
 		double Trace()
 		{
-			tr=0;
-			for (i=0;i<wide;i++)
+			element tr=0;
+			for (int i=0;i<wide;i++)
 				tr+=table[i][i];
 			return(tr);
 		};
@@ -79,50 +76,63 @@ class Matrix
 		Matrix Trans()
 		{
 			Matrix a(wide,1);
-			for (i=0;i<wide;i++)
-				for(j=0;j<wide;j++)
+			for (int i=0;i<wide;i++)
+				for(int j=0;j<wide;j++)
 					a.table[i][j]=table[j][i];
 			return(a);
 		};
-
-		double Determ()
-		{
+		
+		double ToDiag(const Matrix<element>& a)
+		{	
 			double det=1;
 			int i,j,k,p;
-			Matrix<element> table1(*this);
-			for(i=0;i<wide;i++)
-				for(j=i+1;j<wide;j++)
-					table1.table[i][j]=table[i][j];
+			element co,b;
+			
 			for(i=0;i<wide;i++)
 				for(j=i+1;j<wide;j++)
 				{
-					if(table1.table[i][i]==0)
+					if(table[i][i]==0)
 					{
 						p=i;
-						while((table1.table[p][i]==0)&&(p+1<wide)){p++;};
+						while((table[p][i]==0)&&(p+1<wide)){p++;};
 						for(k=0;k<wide;k++)
 						{
-							co=table1.table[p][i];
-							table1.table[p][k]=table1.table[i][k];
-							table1.table[i][k]=co;
+							co=table[p][i];
+							table[p][k]=table[i][k];
+							table[i][k]=co;
+							co=a.table[p][i];
+							a.table[p][k]=a.table[i][k];
+							a.table[i][k]=co;
 						};
 						det*=-1;
-					};	
-					if(table1.table[i][i]!=0)
+					}	
+					if(table[i][i]!=0)
 					{
-						b=table1.table[j][i]/table1.table[i][i];
+						b=table[j][i]/table[i][i];
 						for(k=i;k<wide;k++)
-							table1.table[j][k]-=b*table1.table[i][k];
-					};
-				};
-			for(i=0;i<wide;i++)
+						{	
+							a.table[j][k]-=b*a.table[i][k];
+							table[j][k]-=b*table[i][k];
+						}
+					}
+				}
+	
+			return(det);
+		}
+
+		double Determ()
+		{
+			Matrix<element> table1(*this);
+			double det=table1.ToDiag(*this);
+			
+			for(int i=0;i<wide;i++)
 				det*=table1.table[i][i];
 			return(det);	
 		};
 			
 		Matrix Reversed()
 		{
-			int i,j,k,p; double det;
+			int i,j,k,p; double det; element co,b;
 			Matrix<element> edinst(wide,1);
 			Matrix<element> table1(*this);
 			for(i=0;i<wide;i++)
@@ -135,105 +145,27 @@ class Matrix
 					{
 						edinst.table[i][j]=0;
 					};
+
+			table1.ToDiag(edinst);
+
+			edinst=edinst.Trans();
+			table1=table1.Trans();
+			
+			table1.ToDiag(edinst);
+			
 			for(i=0;i<wide;i++)
-				for(j=i+1;j<wide;j++)
-				{
-					if(table1.table[i][i]==0)
-					{
-						p=i;
-						while( (table1.table[p][i]==0) && (p+1<wide) )
-						{
-							p++;
-						};
-						for(k=0;k<wide;k++)
-						{
-							co=table1.table[p][i];
-							table1.table[p][k]=table1.table[i][k];
-							table1.table[i][k]=co;
-							co=edinst.table[p][i];
-							edinst.table[p][k]=edinst.table[i][k];
-							edinst.table[i][k]=co;
-						};
-					det*=-1;
-					};
-					if(table1.table[i][i]!=0)
-					{	
-						b=table1.table[j][i]/table1.table[i][i];
-						for(k=i;k<wide;k++)
-						{
-							table1.table[j][k]-=b*table1.table[i][k];
-							edinst.table[j][k]-=b*edinst.table[i][k];
-						};	
-					};	
-				};	
-			for(i=0;i<wide;i++)
-				det*=table1.table[i][i];
-				element **revers=new element*[wide];
+				if(table1.table[i][i]!=0)
+					edinst.table[i][i]*=1/table1.table[i][i];
 			
-				for(i=0;i<wide;i++)
-					revers[i]=new double[wide];
-
-				for (i=0;i<wide;i++)
-					for(j=0;j<wide;j++)
-						revers[i][j]=table1.table[j][i];
-			
-				element **reversedin=new element*[wide];
-
-				for(i=0;i<wide;i++)
-					reversedin[i]=new double[wide];
-
-				for (i=0;i<wide;i++)
-					for(j=0;j<wide;j++)
-						reversedin[i][j]=edinst.table[j][i];
-
-				for(i=0;i<wide;i++)
-					for(j=i+1;j<wide;j++)
-						edinst.table[i][j]=reversedin[i][j];
-
-				for(i=0;i<wide;i++)
-					for(j=i+1;j<wide;j++)
-					{
-						if(revers[i][i]==0)
-						{
-							p=i;
-							while( (revers[p][i]==0) && (p+1<wide) )
-							{
-								p++;
-							};
-							for(k=0;k<wide;k++)
-							{
-								co=revers[p][i];
-								revers[p][k]=revers[i][k];
-								revers[i][k]=co;
-								co=edinst.table[p][i];
-								edinst.table[p][k]=edinst.table[i][k];		
-								edinst.table[i][k]=co;
-							};
-						};
-						if(revers[i][i]!=0)
-						{
-							b=revers[j][i]/revers[i][i];
-							for(int k=i;k<wide;k++)
-							{
-								revers[j][k]-=b*revers[i][k];
-								edinst.table[j][k]-=b*edinst.table[i][k];
-							};
-						};
-					}
-				for(i=0;i<wide;i++)
-					if(revers[i][i]!=0)
-						edinst.table[i][i]*=1/revers[i][i];
-			
-				if(det==0){printf("No reverced matrix");} else
-
-				return(edinst);
+			return(edinst);
 		};
-			
+		
 		Matrix operator*(Matrix & other)
 		{
+			element b;
 			Matrix mult(wide,1);
-			for (i=0;i<wide;i++)
-				for(j=0;j<wide;j++)
+			for (int i=0;i<wide;i++)
+				for(int j=0;j<wide;j++)
 				{
 					b=0;
 					for(int a=0;a<wide;a++)
@@ -246,8 +178,8 @@ class Matrix
 		Matrix operator+(Matrix & other)
 		{
 			Matrix sum(wide,1);
-			for (i=0;i<wide;i++)
-				for(j=0;j<wide;j++)
+			for (int i=0;i<wide;i++)
+				for(int j=0;j<wide;j++)
 					sum.table[i][j]=table[i][j]+other.table[i][j];
 			return(sum);
 		};
@@ -257,44 +189,22 @@ class Matrix
 			return(table[i]);
 		};
 
-		friend ostream& operator<<(ostream &out,Matrix &x)
-		{
-	        	for(int i=0;i<x.wide;i++)
-			{
-			        for(int j=0;j<x.wide;j++)
-			            out<<x.table[i][j]<<" ";
-        				out<<endl;
-    			}
-    			out<<x.wide<<" "<<x.wide;
-		return out;
-		}
 
 	
 		
 
 };
 
-/*void PrintMatrix(Matrix<double>& a)
-{
-   for (int i = 0; i < a.wide ; ++ i)
-   {
-      for (int j = 0; j < a.wide ; ++ j)
-         printf ("%5.3lf ", a.table[i][j]);
-      puts ("");
-   }
-}
-*/
+
+
 int main(){
 	int i,j; 
-	Matrix<double> odin(50);
+	Matrix<double> odin(3);
 	odin.PrintTable();
+	printf("%.2lf" , odin.Determ());
 	printf("\n Обратная матрица \n");
-	Matrix<double> odint(odin.Reversed());
-	odint.PrintTable();
-	//Matrix<double> odint(odin);
-	//cout << odin;
-	//odin.PrintTable();
+	//Matrix<double> odint(odin.Reversed());
+	//odint.PrintTable();	
 	
-	
-return(0);
+	return(0);
 };
