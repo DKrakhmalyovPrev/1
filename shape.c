87baf3c36647d37e7a64ec2d798d6a844ec118c6
fredy4eg <fredy97@mail.ru>
2015-04-11 15:08:58 +0300

    Update shape.c

--- shape.c
+++ shape.c
@@ -12,13 +12,20 @@ class Point
 	private:		
 		double x1,y1;
 	public:
-		Point()
+		Point(){};
+
+		Point(double j,double k): x1(j), y1(k){};
+		
+		void ScanPoint()
 		{
-			cout<<"Введите координаты точки\n";
 			cin>>x1>>y1;
 		}
 		
-		Point(double j,double k): x1(j), y1(k){};
+		void PrintPoint()
+		{
+			cout<<x1<<" "<<y1;
+		}
+		
 		
 };
 
@@ -29,52 +36,62 @@ class Segment
 	private:		
 		double x1,x2,y1,y2, A, B, C;
 	public:
-		Segment()
-		{
-			cout<<"Введите точки отрезка\n";
-			cin>>x1>>y1>>x2>>y2;
-		}
+		Segment(){}
 		
 		Segment(double i,double j,double k,double l): x1(i), y1(j), x2(k), y2(l){};
 	
-	void Lane()
-	{
-		double k=(y2-y1)/(x2-x1);
-		double b=(x2*y1-x1*y2)/(x2-x1);
-		A=k;
-		B=-1;
-		C=-b;
-	}
-	
-	bool SegmentCut(Segment& r, Segment& s)
-	{
-		double x,y;
-		r.Lane();
-		s.Lane();
-		y=( (s.C-r.C * (s.A/r.A)) / (s.B-r.B * (s.A/r.A)) );
-		x=( r.C - r.B*y ) / r.A;
-		cout<<x<<" "<<y;
-		if( ((r.x1*s.A+r.y1*s.B-s.C) * (r.x2*s.A+r.y2*s.B-s.C) >= 0) && ((s.x1*r.A+s.y1*r.B-r.C) * (s.x2*r.A+s.y2*r.B-r.C) >= 0)) 
-		{return(0);} 
-		else {return(1);};
-	}	
-	
-	const void SegmentPoint(Segment& seg, Point& po)
-	{
-		seg.Lane();
-		if( (seg.A*po.x1 + seg.B*po.y1 == seg.C) && ( (po.x1-seg.x1) * (po.x1-seg.x2) <= 0) ){printf("Лежит");}else{printf("Не лежит");};
-	} 
-	
-	const void LanePoint(Segment& seg, Point& po)
-	{
-		seg.Lane();
-		if( seg.A*po.x1 + seg.B*po.y1 > seg.C){cout<<"Ниже";};
-		if( seg.A*po.x1 + seg.B*po.y1 < seg.C){cout<<"Выше";}; 
-		if( seg.A*po.x1 + seg.B*po.y1 == seg.C){cout<<"На прямой";};
-	}
 
+		void ScanSegment()
+		{
+			cin>>x1>>y1>>x2>>y2;
+		}
+		
+		void PrintSegment()
+		{
+			cout<<x1<<" "<<y1<<" "<<x2<<" "<<y2;
+		}
+
+		void Lane()
+		{
+			double k=(y2-y1)/(x2-x1);
+			double b=(x2*y1-x1*y2)/(x2-x1);
+			A=k;
+			B=-1;
+			C=-b;
+		}
+		
+		bool SegmentCut(Segment& r, Segment& s)
+		{
+			double x,y;
+			r.Lane();
+			s.Lane();
+			y=( (s.C-r.C * (s.A/r.A)) / (s.B-r.B * (s.A/r.A)) );
+			x=( r.C - r.B*y ) / r.A;
+			cout<<x<<" "<<y;
+			if( ((r.x1*s.A+r.y1*s.B-s.C) * (r.x2*s.A+r.y2*s.B-s.C) >= 0) && ((s.x1*r.A+s.y1*r.B-r.C) * (s.x2*r.A+s.y2*r.B-r.C) >= 0)) 
+			{return(0);} 
+			else {return(1);};
+		}	
+		
+		const bool SegmentPoint(Segment& seg, Point& po)
+		{
+			seg.Lane();
+			if( (seg.A*po.x1 + seg.B*po.y1 == seg.C) && ( (po.x1-seg.x1) * (po.x1-seg.x2) <= 0) )
+				Return(1);
+			else
+				Return(0);
+		} 
+		
+		const int LanePoint(Segment& seg, Point& po)
+		{
+			seg.Lane();
+			if( seg.A*po.x1 + seg.B*po.y1 > seg.C) return(-1);//Выше
+			if( seg.A*po.x1 + seg.B*po.y1 < seg.C) return(1); //Ниже
+			if( seg.A*po.x1 + seg.B*po.y1 == seg.C) return(0);//На прямой
+		}
 	
-};
+		
+};	
 
 class Vector
 {
@@ -84,14 +101,19 @@ class Vector
 		double  xst, yst, xend, yend; 
 	public:
 		
-		Vector()
-		{
-			cout<<"Введите координаты начала и конца";
-			cin>>xst>>yst>>xend>>yend;
-		}
-
+		Vector(){};
+		
 		Vector(double i,double j,double k,double l): xst(i), yst(j), xend(k), yend(l){};
 	
+		void ScanVector()
+		{
+			cin>>xst>>yst>>xend>>yend;
+		}
+		
+		void PrintPVector()
+		{
+			cout<<xst<<" "<<yst<<" "<<xend<<" "<<yend;
+		}
 
 		Vector Reverce()
 		{
